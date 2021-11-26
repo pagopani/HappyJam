50420ff0aa430b0a08604a5575dccded53a63e02
pagopani <panipani4772@icloud.com>
2021-11-26 10:44:34 +0900

    プロジェクト ファイルを追加します。

---
+++ HappyJam/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\r4a2\Desktop\sotuken\HappyJam\HappyJam\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
