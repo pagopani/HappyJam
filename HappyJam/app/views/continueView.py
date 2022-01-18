75ef1e16f7ab008e650ad045dd09091678766c52
yurikb8 <yurikb8@gmail.com>
2022-01-18 17:48:33 +0900

    webmで録画後、mp4に変換(激重)

--- HappyJam/app/views/continueView.py
+++ HappyJam/app/views/continueView.py
@@ -18,39 +18,41 @@ class continueView(TemplateView):
     
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
+            u_id = request.session['uid']
+            genre = request.session['genre']
+            inst = request.session['inst']
+            filename = str(u_id) + "/" + genre + "/" +inst
+            
             if request.is_ajax():
                 #Ajax 処理を別メソッドに切り離す
                 if request.FILES['movie_record']:
                     movie = request.FILES['movie_record']
                     fileobject = FileSystemStorage()
-                    u_id = request.session['uid']
-                    genre = request.session['genre']
-                    inst = request.session['inst']
-                    filename = str(u_id) + "/" + genre + "/" +inst #ファイル名をmedia/uid/genre/inst.mp4にする
-                    fileobject.save((filename+".mp4"),movie) #保存
-
-                    #Movieテーブルに登録
-                    m_data= Movie(movie_path=(filename+".mp4")) #movie_pathにuid/genre/inst.mp4を保存 
-                    m_data.save()
-
-                    #音楽抽出
-                    stream = ffmpeg.input("./media/"+filename+".mp4") 
-                    stream = ffmpeg.output(stream, ("./media/"+filename+".wav")) 
-                    ffmpeg.run(stream)
-
-                    #Musicテーブルに登録&Movieテーブルを更新
-                    music = Music(music = (filename + ".wav"))
-                    music.save()
-                    m_data.music_id = music
-                    m_data.save()
-
-                    #Singleテーブルを更新
-                    instrument = Instrument.objects.get(instrument_name=inst)
-                    s_data = Single.objects.select_related('instrument_id').get(uid=u_id,instrument_id=instrument)
-                    s_data.movie_id = m_data
-                    s_data.save()
+                    fileobject.save((filename+".webm"),movie) #保存
                     return HttpResponse("ajax is done")
 
+            #動画変換
+            stream = ffmpeg.input("./media/"+filename+".webm") 
+            stream = ffmpeg.output(stream, ("./media/"+filename+".mp4")) 
+            ffmpeg.run(stream)
+                    
+            #音楽抽出
+            stream = ffmpeg.input("./media/"+filename+".mp4") 
+            stream = ffmpeg.output(stream, ("./media/"+filename+".wav")) 
+            ffmpeg.run(stream)
+
+            #Musicテーブルに登録&Movieテーブルも登録
+            music = Music(music = (filename + ".wav"))
+            music.save()
+            m_data = Movie(movie_path=(filename + ".mp4"),music_id=music)
+            m_data.save()
+
+            #Singleテーブルを更新
+            instrument = Instrument.objects.get(instrument_name=inst)
+            s_data = Single.objects.select_related('instrument_id').get(uid=u_id,instrument_id=instrument)
+            s_data.movie_id = m_data
+            s_data.save()
+            
             return render(request,'app/Continue.html')
 
 Continue = continueView.as_view()
