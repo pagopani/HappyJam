ecf64c22eeebe2618826b4260700534edc1cdc3f
yurikb8 <yurikb8@gmail.com>
2022-02-03 14:11:09 +0900

    modelのインポート追加

--- HappyJam/app/views/genreView.py
+++ HappyJam/app/views/genreView.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from ..models import User
 
 
 class genreView(TemplateView):
